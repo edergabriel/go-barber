b550fc9db7cd35e761b4b5805cfec3a23de32296
Eder Gabriel <eder1602@outlook.com>
2020-12-15 23:55:46 -0300

    Adicionando Repositório

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,27 +2,30 @@ import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 import Appointment from '../model/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentRepository'
+import AppointmentRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments: Appointment[] = [];
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all()
+
+  return response.status(200).json(appointments)
+})
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const dateAppointment = startOfHour(parseISO(date));
 
-    const findAppointmentSameDate = appointments.find(appointment =>
-      isEqual(dateAppointment, appointment.date),  
-    );
+    const findAppointmentSameDate = appointmentsRepository.findByDate(dateAppointment)
 
     if(findAppointmentSameDate) {
         return response.status(400).json({ message: "Agendamento com horário marcado!"})
     } 
 
-    const appointment = new Appointment(provider, dateAppointment);
-
-    appointments.push(appointment)
+    const appointment = appointmentsRepository.create(provider, dateAppointment);
  
     return response.json(appointment);
 });
