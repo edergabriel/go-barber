d4ab5fd72cfbb678cac4f2d01ede648e104d691f
Eder Gabriel <eder1602@outlook.com>
2020-12-29 21:53:15 -0300

    Atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,7 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
-
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import  ensureAuthenticated  from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -31,8 +31,21 @@ try {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    console.log(request.file);
-    return response.status(200).json({ ok: true});
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename
+        });
+
+        delete user.password;
+
+        return response.status(200).json(user);
+    } catch(err) {
+        return response.status(400).json({ error: err.message })
+    }
+
 });
 
 export default usersRouter;
