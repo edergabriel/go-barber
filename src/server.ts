bfa2a8afd73ab6d044c2991fe7eaaf0340f5cc4c
Eder Gabriel <eder1602@outlook.com>
2020-12-29 21:58:47 -0300

    Rota para arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,11 @@ import routes from './routes';
 const app = express();
 
 import './database';
+import uploadConfig from './config/upload';
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory))
 
 app.listen(3333, () => {
     console.log("Server started on port 3333!");
