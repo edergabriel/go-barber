3f2134fa403e937f9dfc41f1a4ff4fea137cac61
Eder Gabriel <eder1602@outlook.com>
2021-03-24 23:53:07 -0300

    Reescrevendo Repositórios e Dependency Inversion Principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
-import AppointmentsRepository from '../../../repositories/AppointmentRepository'
+import AppointmentsRepository from '../../typeorm/repositories/AppointmentRepository'
 import CreateAppointmentService from '../../../services/CreateAppointmentService';
-import { getCustomRepository } from 'typeorm';
 import  ensureAuthenticated from '../../../../users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+//appointmentsRouter.get('/', async (request, response) => {
+  //const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  const appointments = await appointmentsRepository.find()
+ // const appointments = await appointmentsRepository.find()
 
-  return response.status(200).json(appointments)
-})
+//  return response.status(200).json(appointments)
+//})
 
 appointmentsRouter.post('/', async (request, response) => {
 
@@ -23,7 +23,9 @@ appointmentsRouter.post('/', async (request, response) => {
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,32 +1,32 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentRepository';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentRepository';
 import AppError from '../../../shared/errors/AppError';
-
-interface Request {
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+interface IRequest {
     provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    constructor(private appointmentsRepository: IAppointmentsRepository) {}
+
+    public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
         const dateAppointment = startOfHour(date);
 
-        const findAppointmentSameDate = await appointmentsRepository.findByDate(dateAppointment)
+        const findAppointmentSameDate = await this.appointmentsRepository.findByDate(dateAppointment)
 
         if(findAppointmentSameDate) {
             throw new AppError("Agendamento com horário marcado!")
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await this.appointmentsRepository.create({
             provider_id,
             date: dateAppointment
         })
-
-        await appointmentsRepository.save(appointment);
 
         return appointment
     }
